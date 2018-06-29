5702cdab322ab23f66882d58f7e6c2882cbe1bd3
Sheep-y <your.sheepy@gmail.com>
2018-06-29 09:03:03 +0800

    Move out test codes into new file.

--- AttackImprovementMod/src/Mod.cs
+++ AttackImprovementMod/src/Mod.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Threading;
 
 namespace Sheepy.AttackImprovementMod {
 
@@ -24,43 +23,8 @@ namespace Sheepy.AttackImprovementMod {
       internal static string LogDir = "";
       internal static HarmonyInstance harmony = HarmonyInstance.Create( "io.github.Sheep-y.AttackImprovementMod" );
 
-      private static void checkNaN ( float count ) {
-         float block = 2f/5f, add = count*block, max = 100000;
-         for ( int i = 0 ; i <= max ; i++ ) {
-            float strength = add + ((float)i)*block/max;
-            if ( strength > 1.9999f ) continue;
-            for ( int j = 0 ; j <= 10000 ; j++ ) {
-               float acc = ((float)j)/10000f, corrected = RollCorrection.ReverseRollCorrection( acc, strength );
-               if ( float.IsNaN( corrected ) ) Console.WriteLine( corrected + " = " + acc + ", " + strength );
-            }
-         }
-      }
-
-      static void Main () { // Sometimes I run quick tests as a console app here
-         /*
-         new Thread( () => checkNaN(0) ).Start();
-         new Thread( () => checkNaN(1) ).Start();
-         new Thread( () => checkNaN(2) ).Start();
-         new Thread( () => checkNaN(3) ).Start();
-         new Thread( () => checkNaN(4) ).Start();
-         /**
-         foreach ( MemberInfo e in typeof( Team ).GetMembers( BindingFlags.NonPublic | BindingFlags.Instance ) )
-            Console.WriteLine( e );
-         FieldInfo m1 = typeof( Team ).GetField( "streakBreakingValue", BindingFlags.NonPublic | BindingFlags.Instance );
-         Console.WriteLine( m1 );
-         /**/ /**
-         for ( int i = 0 ; i < 20 ; i++ ) {
-            float roll = i * 0.05f;
-            float rev1 = RollCorrection.ReverseRollCorrection( roll, 0.5f  ), rrev1 = RollCorrection.CorrectRoll( rev1, 0.5f ),
-                  rev2 = RollCorrection.ReverseRollCorrection( roll, 1f    ), rrev2 = RollCorrection.CorrectRoll( rev2, 1f ),
-                  rev3 = RollCorrection.ReverseRollCorrection( roll, 1.9999f), rrev3 = RollCorrection.CorrectRoll( rev3, 1.9999f );
-            Console.WriteLine( string.Format( "{0:0.00} => [Half correction] {1:0.0000}, Re-rev {2:0.0000}   [Full] {3:0.0000}, Re-rev {4:0.0000}   [Double] {5:0.0000}, Re-rev {6:0.0000}",
-               new object[]{ roll, rev1, rrev1, rev2, rrev2, rev3, rrev3 } ) );
-         } /**/
-         Console.ReadKey();
-      }
-
       public static void Init ( string directory, string settingsJSON ) {
+         // Get log settings
          string logCache = "";
          try {
             Settings = JsonConvert.DeserializeObject<ModSettings>( settingsJSON );
@@ -68,7 +32,6 @@ namespace Sheepy.AttackImprovementMod {
          } catch ( Exception ex ) {
             logCache = string.Format( "Error: Cannot read mod settings, using default: {0}", ex );
          }
-
          if ( Settings.LogFolder.Length <= 0 ) {
             LogDir = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ) + "/";
             logCache += "\nLog folder set to " + LogDir + ". If that fails, fallback to " + FALLBACK_LOG_DIR + "." ;
@@ -76,7 +39,7 @@ namespace Sheepy.AttackImprovementMod {
          DeleteLog( LOG_NAME );
          Log( logCache );
 
-         // Need a proper version parsing routine. Next time.
+         // Detect game features. Need a proper version parsing routine. Next time.
          if ( ( VersionInfo.ProductVersion + ".0.0" ).Substring( 0, 4 ) == "1.0." ) {
             GameUseClusteredCallShot = GameHitLocationBugged = true;
             Log( "Game is 1.0.x (Clustered Called Shot, Hit Location bugged)" );
@@ -88,6 +51,7 @@ namespace Sheepy.AttackImprovementMod {
          }
          Log();
 
+         // Patching
          try {
             if ( Settings.ShowRealMechCalledShotChance || Settings.ShowRealVehicleCalledShotChance || Settings.ShowHeatAndStab ) {
                patchClass = typeof( Mod );
@@ -118,32 +82,26 @@ namespace Sheepy.AttackImprovementMod {
          return new HarmonyMethod( mi );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch ( Type patchedClass, string patchedMethod, string prefix, string postfix ) {
          Patch( patchedClass, patchedMethod, BindingFlags.Public | BindingFlags.Instance, (Type[]) null, prefix, postfix );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch ( Type patchedClass, string patchedMethod, Type parameterType, string prefix, string postfix ) {
          Patch( patchedClass, patchedMethod, BindingFlags.Public | BindingFlags.Instance, new Type[]{ parameterType }, prefix, postfix );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch ( Type patchedClass, string patchedMethod, Type[] parameterTypes, string prefix, string postfix ) {
          Patch( patchedClass, patchedMethod, BindingFlags.Public | BindingFlags.Instance, parameterTypes, prefix, postfix );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch ( Type patchedClass, string patchedMethod, BindingFlags flags, string prefix, string postfix ) {
          Patch( patchedClass, patchedMethod, flags, (Type[]) null, prefix, postfix );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch ( Type patchedClass, string patchedMethod, BindingFlags flags, Type parameterType, string prefix, string postfix ) {
          Patch( patchedClass, patchedMethod, flags, new Type[]{ parameterType }, prefix, postfix );
       }
 
-      /* Find and patch a method with prefix and/or postfix. */
       internal static void Patch( Type patchedClass, string patchedMethod, BindingFlags flags, Type[] parameterTypes, string prefix, string postfix ) {
          MethodInfo patched;
          if ( parameterTypes == null )
---
+++ AttackImprovementMod/src/Test.cs
@@ -0,0 +1,53 @@
+﻿using BattleTech;
+using System;
+using System.Reflection;
+using System.Threading;
+
+namespace Sheepy.AttackImprovementMod {
+   /* Colllection of test code that I don't want to cluster the main code */
+   class Test {
+
+      static void Main () { // Sometimes I run quick tests as a console app here
+         // runReverseRollTest();
+         /**/
+         foreach ( MemberInfo e in typeof( Team ).GetMembers( BindingFlags.NonPublic | BindingFlags.Instance ) )
+            Console.WriteLine( e );
+         FieldInfo m1 = typeof( Team ).GetField( "streakBreakingValue", BindingFlags.NonPublic | BindingFlags.Instance );
+         Console.WriteLine( m1 ); /**/
+
+         Console.ReadKey();
+      }
+
+      internal static void listRollCorrection () {
+         for ( int i = 0 ; i < 20 ; i++ ) {
+            float roll = i * 0.05f;
+            float rev1 = RollCorrection.ReverseRollCorrection( roll, 0.5f  ), rrev1 = RollCorrection.CorrectRoll( rev1, 0.5f ),
+                  rev2 = RollCorrection.ReverseRollCorrection( roll, 1f    ), rrev2 = RollCorrection.CorrectRoll( rev2, 1f ),
+                  rev3 = RollCorrection.ReverseRollCorrection( roll, 1.9999f), rrev3 = RollCorrection.CorrectRoll( rev3, 1.9999f );
+            Console.WriteLine( string.Format( "{0:0.00} => [Half correction] {1:0.0000}, Re-rev {2:0.0000}   [Full] {3:0.0000}, Re-rev {4:0.0000}   [Double] {5:0.0000}, Re-rev {6:0.0000}",
+               new object[]{ roll, rev1, rrev1, rev2, rrev2, rev3, rrev3 } ) );
+         }
+      }
+
+      internal static void runReverseRollTest () {
+         new Thread( () => checkReverseNaN(0) ).Start();
+         new Thread( () => checkReverseNaN(1) ).Start();
+         new Thread( () => checkReverseNaN(2) ).Start();
+         new Thread( () => checkReverseNaN(3) ).Start();
+         new Thread( () => checkReverseNaN(4) ).Start();
+      }
+
+      private static void checkReverseNaN ( float count ) {
+         float block = 2f/5f, add = count*block, max = 100000;
+         for ( int i = 0 ; i <= max ; i++ ) {
+            float strength = add + ((float)i)*block/max;
+            if ( strength > 1.9999f ) continue;
+            for ( int j = 0 ; j <= 10000 ; j++ ) {
+               float acc = ((float)j)/10000f, corrected = RollCorrection.ReverseRollCorrection( acc, strength );
+               if ( float.IsNaN( corrected ) ) Console.WriteLine( corrected + " = " + acc + ", " + strength );
+            }
+         }
+      }
+
+   }
+}
