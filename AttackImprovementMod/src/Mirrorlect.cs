f1a54a7856e6009c734ea38ee04e2c114e6c96f2
Sheep-y <your.sheepy@gmail.com>
2018-08-05 23:05:03 +0800

    Add property and method support. Add type alias. Add auto-namespace.

--- AttackImprovementMod/src/Mirrorlect.cs
+++ AttackImprovementMod/src/Mirrorlect.cs
@@ -1,46 +1,28 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Sheepy.CSharpUtils {
+namespace Sheepy.Mirrorlect {
+   using static System.Diagnostics.SourceLevels;
    using static System.Reflection.BindingFlags;
 
    public class Mirrorlect {
 
+      // Both are locked to (this)
       public readonly List<Assembly> Assemblies = new List<Assembly>();
-      public Action<string> WarningHandler = ( msg ) => Console.WriteLine( msg );
+      public readonly Dictionary<Assembly, HashSet<string>> Namespaces = new Dictionary<Assembly, HashSet<string>>();
+      public Action<SourceLevels,string,object[]> WarningHandler = ( level, msg, args ) => {
+         if ( level <= Warning ) Console.WriteLine( String.Format( msg, args ) );
+      };
 
       public Mirrorlect () {
-         Assemblies.Add( GetType().Assembly );
-      }
-      
-      public interface IMemberProxy <T> {
-         IMemberProxy<T> Of( object subject );
-         T Get();
-         void Set( T value );
-      }
-
-      public abstract class MemberProxy <T> : IMemberProxy <T> {
-         public readonly MemberInfo Member;
-         protected object subject;
-         public MemberProxy ( MemberInfo info ) { Member = info; }
-         public IMemberProxy<T> Of( object subject ) { 
-            MemberProxy<T> cloned = (MemberProxy<T>) MemberwiseClone();
-            cloned.subject = subject;
-            return cloned;
-         }
-         public abstract T Get();
-         public abstract void Set( T value );
-      }
-
-      public class FieldProxy <T> : MemberProxy <T> {
-         public FieldInfo Field { get => (FieldInfo) Member; }
-         public FieldProxy ( FieldInfo info ) : base( info ) {}
-         public override T Get() { return (T) Field.GetValue( subject ); }
-         public override void Set( T value ) { Field.SetValue( subject, value ); }
+         AddAssembly( GetType().Assembly );
       }
 
       class MemberPart {
@@ -76,9 +58,22 @@ namespace Sheepy.CSharpUtils {
 
       public static Mirrorlect Mirror { get; } = new Mirrorlect();
 
-      public Mirrorlect AddAssembly ( Type typeInAssembly ) { return AddAssembly( typeInAssembly.Assembly ); }
-      public Mirrorlect AddAssembly ( Assembly assembly ) { lock( Assemblies ) {
-         if ( ! Assemblies.Contains( assembly ) ) Assemblies.Add( assembly );
+      public Mirrorlect AddAssembly ( Type typeInAssembly, string[] namespaces = null ) { return AddAssembly( typeInAssembly.Assembly, namespaces ); }
+      public Mirrorlect AddAssembly ( Assembly assembly, string[] namespaces = null ) { lock ( this ) {
+         if ( assembly == null ) throw new ArgumentNullException();
+         if ( ! Assemblies.Contains( assembly ) ) {
+            Assemblies.Add( assembly );
+            Log( Information, "Added assembly {0}", assembly );
+         }
+         if ( namespaces == null ) {
+            namespaces = assembly.GetTypes().Select( e => e.Namespace ).ToArray();
+            Log( Information, "Auto-adding all namespaces in assembly {0}", assembly );
+         } else if ( namespaces.Length <= 0 ) 
+            return this;
+         else
+            Log( Verbose, "Adding {1} namespaces to assembly {0}", assembly, namespaces );
+         if ( ! Namespaces.ContainsKey( assembly ) ) Namespaces.Add( assembly, new HashSet<string>() );
+         Namespaces[ assembly ].UnionWith( namespaces );
          return this;
       } }
 
@@ -90,6 +85,14 @@ namespace Sheepy.CSharpUtils {
          throw new NotImplementedException( "Method parameter matching not implemented" );
       }
 
+      private void Log( SourceLevels level, string message, params object[] args ) {
+         if ( WarningHandler == null ) return;
+         try { WarningHandler( level, message, args ); } catch ( Exception ) { }
+      }
+
+      //private static Dictionary<string,Type> typeCache = new Dictionary<string, Type>();
+      //private static Dictionary<string,WeakReference> parserCache = new Dictionary<string,WeakReference>();
+
       private MemberPart MatchMember ( TextParser state ) {
          MemberPart lastMember = null;
          do {
@@ -99,7 +102,7 @@ namespace Sheepy.CSharpUtils {
             if ( state.IsEmpty || state.Next == '(' ) break;
             state.Take( '.' );
          } while ( true );
-         if ( lastMember == null ) state.Equals( "Identifier expected" );
+         if ( lastMember == null ) state.Error( "Identifier expected" );
          return lastMember;
       }
 
@@ -109,16 +112,103 @@ namespace Sheepy.CSharpUtils {
          if ( type == null ) return null;
          MemberInfo[] info = type.GetMember( member.MemberName, Public | Instance | Static );
          if ( info == null || info.Length <= 0 ) info = type.GetMember( member.MemberName, NonPublic | Instance | Static );
-         return new FieldProxy<T>( (FieldInfo) info[0] );
+         if ( info.Length > 1 ) Log( Warning, "Multiple matches ({1}) found for {0}", member, info.Length );
+         MemberInfo result = info[0];
+         switch ( result.MemberType ) {
+         case MemberTypes.Field:
+            return new FieldProxy<T>( (FieldInfo) result );
+         case MemberTypes.Property:
+            return new PropertyProxy<T>( (PropertyInfo) result );
+         case MemberTypes.Method:
+            return new MethodProxy<T>( (MethodInfo) result );
+         default:
+            throw new NotSupportedException( result.MemberType + " not implemented" );
+         }
       }
  
       private Type GetType ( MemberPart member ) {
+         string fullname = member.ToString();
+         if ( fullname.IndexOf( '.' ) < 0 ) {
+            if ( shortTypes.Count == 0 ) BuildTypeMap();
+            if ( shortTypes.TryGetValue( fullname, out Type shortType ) ) return shortType;
+         }
+         if ( fullname.IndexOf( '.' ) > 0 )
+            foreach ( Assembly assembly in Assemblies ) {
+               Type type = assembly.GetType( fullname );
+               if ( type != null ) return type;
+            }
          foreach ( Assembly assembly in Assemblies ) {
-            Type type = assembly.GetType( member.ToString() );
-            if ( type != null ) return type;
+            if ( ! Namespaces.TryGetValue( assembly, out HashSet<string> names ) ) continue;
+            foreach ( string name in names ) {
+               Type type = assembly.GetType( name + "." + fullname );
+               if ( type != null ) return type;
+            }
          }
          return null;
       }
+
+      private static Dictionary<string, Type> shortTypes = new Dictionary<string, Type>();
+
+      private static void BuildTypeMap () { lock( shortTypes ) {
+         if ( shortTypes.Count > 0 ) return;
+         using ( var provider = new CSharpCodeProvider() ) {
+            Assembly mscorlib = Assembly.GetAssembly( typeof( int ) );
+            foreach ( Type type in mscorlib.GetTypes() ) {
+               if ( type.Namespace != "System" ) continue;
+               var typeRef = new CodeTypeReference(type);
+               var csTypeName = provider.GetTypeOutput(typeRef);
+               if ( csTypeName.IndexOf( '.' ) >= 0 ) continue;
+               shortTypes.Add( csTypeName, type );
+            }
+         }
+      } }
+   }
+
+   public interface IMemberProxy <T> {
+      IMemberProxy<T> Of( object subject );
+      T Get( params object[] index );
+      T Call( params object[] parameters );
+      void Set( T value, params object[] index );
+   }
+
+   public abstract class MemberProxy <T> : IMemberProxy <T> {
+      public readonly MemberInfo Member;
+      protected object subject;
+      public MemberProxy ( MemberInfo info ) { Member = info; }
+      public IMemberProxy<T> Of( object subject ) { 
+         MemberProxy<T> cloned = (MemberProxy<T>) MemberwiseClone();
+         cloned.subject = subject;
+         return cloned;
+      }
+      public abstract T Get( params object[] index );
+      public abstract T Call( params object[] parameters );
+      public abstract void Set( T value, params object[] index );
+   }
+
+   public class FieldProxy <T> : MemberProxy <T> {
+      public FieldInfo Field { get => (FieldInfo) Member; }
+      public FieldProxy ( FieldInfo info ) : base( info ) {}
+      public override T Get( params object[] index ) { return (T) Field.GetValue( subject ); }
+      public override T Call( params object[] parameters ) { return Get( parameters ); }
+      public override void Set( T value, params object[] index ) { Field.SetValue( subject, value ); }
+   }
+
+   public class PropertyProxy <T> : MemberProxy <T> {
+      public PropertyInfo Property { get => (PropertyInfo) Member; }
+      public PropertyProxy ( PropertyInfo info ) : base( info ) {}
+      public override T Get( params object[] index ) { return (T) Property.GetValue( subject, index ); }
+      public override T Call( params object[] parameters ) { return Get( parameters ); }
+      public override void Set( T value, params object[] index ) { Property.SetValue( subject, value, index ); }
+   }
+
+   public class MethodProxy <T> : MemberProxy <T> {
+      public MethodInfo Method { get => (MethodInfo) Member; }
+      public MethodProxy ( MethodInfo info ) : base( info ) {}
+      public override T Get( params object[] index ) { return Call( index ); }
+      public override T Call( params object[] parameters ) {
+         return (T) Method.Invoke( subject, parameters );
+      }
+      public override void Set( T value, params object[] index ) { Call( index ); }
    }
 
    public class TextParser {
