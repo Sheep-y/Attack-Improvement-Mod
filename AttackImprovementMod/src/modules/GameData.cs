62d733c1752a2b17c342b54ddb7825f6f6faf6c6
Sheep-y <your.sheepy@gmail.com>
2018-12-07 01:42:29 +0800

    Add Holly SRM fix (1.2.x only) and fix ER PPC debuff.

--- AttackImprovementMod/src/modules/GameData.cs
+++ AttackImprovementMod/src/modules/GameData.cs
@@ -13,6 +13,7 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
          weaponDefs = new Dictionary<string, WeaponDef>();
          weaponsToFix = new HashSet<string>( new string[]{ 
             "Weapon_LRM_LRM5_2-Delta", "Weapon_LRM_LRM10_2-Delta", "Weapon_LRM_LRM15_2-Delta", // Since Launch
+            "Weapon_SRM_SRM6_1-Holly", "Weapon_SRM_SRM6_2-Holly", // 1.0 to 1.2.1, fixed in 1.3
             "Weapon_Laser_LargeLaserER_2-BlazeFire", "Weapon_PPC_PPCER_0-STOCK", "Weapon_PPC_PPCER_1-MagnaFirestar", "Weapon_PPC_PPCER_2-TiegartMagnum" } ); // Patch 1.3
          Patch( typeof( WeaponDef ), "FromJSON", null, "FixWeaponStats" );
       }
@@ -27,24 +28,39 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
          weaponDefs.Add( id, __instance );
          switch ( id ) {
             case "Weapon_PPC_PPC_0-STOCK":
-               PPC_STATUS = HBS.Util.JSONSerializationUtility.ToJSON<EffectData[]>(__instance.statusEffects);
-               Info( "PPC debuff data cloned." );
+               if ( __instance.statusEffects.Length == 1 ) {
+                  PPC_STATUS = HBS.Util.JSONSerializationUtility.ToJSON( __instance.statusEffects[0] );
+                  Info( "PPC debuff cloned." );
+               } else
+                  Warn( "PPC debuff mismatch." );
                break;
+
             case "Weapon_LRM_LRM5_2-Delta":
             case "Weapon_LRM_LRM10_2-Delta":
             case "Weapon_LRM_LRM15_2-Delta":
                if ( CanFixWeapon( id ) )
                   TryFixProp( id, __instance, "Instability", e => e.Instability == 5 && e.BonusValueA.Contains( "+ 2" ), 4 );
                break;
-            case "Weapon_Laser_LargeLaserER_2":
+
+            case "Weapon_SRM_SRM6_1-Holly":
+            case "Weapon_SRM_SRM6_2-Holly":
+               if ( CanFixWeapon( id ) )
+                  TryFixProp( id, __instance, "AccuracyModifier", e => e.AccuracyModifier > 0 && e.BonusValueB == "", 0 );
+               break;
+
+            case "Weapon_Laser_LargeLaserER_2-BlazeFire":
                if ( CanFixWeapon( id ) )
                   TryFixProp( id, __instance, "BonusValueA", e => e.BonusValueA == "" && e.BonusValueB == "", "+ 5% Crit" );
                break;
+
             case "Weapon_PPC_PPCER_0-STOCK":
             case "Weapon_PPC_PPCER_1-MagnaFirestar":
             case "Weapon_PPC_PPCER_2-TiegartMagnum":
-               if ( CanFixWeapon( id ) )
-                  TryFixProp( id, __instance, "statusEffects", e => e.statusEffects.IsNullOrEmpty(), HBS.Util.JSONSerializationUtility.FromJSON<EffectData[]>(new EffectData[1], PPC_STATUS) );
+               if ( CanFixWeapon( id ) ) {
+                  EffectData debuff = new EffectData();
+                  HBS.Util.JSONSerializationUtility.FromJSON( debuff, PPC_STATUS );
+                  TryFixProp( id, __instance, "statusEffects", e => e.statusEffects.IsNullOrEmpty(), new EffectData[]{ debuff } );
+               }
                break;
          }
       }                 catch ( Exception ex ) { Error( ex ); } }
@@ -66,9 +82,9 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
             prop.SetValue( def, value, null );
             Info( id + " " + propName + " corrected." );
          } else
-            Warn( "Stat or property mismatch; " + id + " not corrected." );
+            Warn( id + " " + propName + " not corrected due to stats or property mismatch." );
       }
 
-      private static string PPC_STATUS = "[{\"durationData\":{\"duration\":1,\"ticksOnActivations\":true,\"useActivationsOfTarget\":true,\"ticksOnEndOfRound\":false,\"ticksOnMovements\":false,\"stackLimit\":0,\"clearedWhenAttacked\":false},\"targetingData\":{\"effectTriggerType\":\"OnHit\",\"triggerLimit\":0,\"extendDurationOnTrigger\":0,\"specialRules\":\"NotSet\",\"effectTargetType\":\"NotSet\",\"range\":0,\"forcePathRebuild\":false,\"forceVisRebuild\":false,\"showInTargetPreview\":true,\"showInStatusPanel\":true},\"effectType\":\"StatisticEffect\",\"Description\":{\"Id\":\"AbilityDefPPC\",\"Name\":\"SENSORS IMPAIRED\",\"Details\":\"[AMT] Difficulty to all of this unit's attacks until its next activation.\",\"Icon\":\"uixSvgIcon_status_sensorsImpaired\"},\"nature\":\"Debuff\",\"statisticData\":{\"appliesEachTick\":false,\"effectsPersistAfterDestruction\":false,\"statName\":\"AccuracyModifier\",\"operation\":\"Float_Add\",\"modValue\":\"1.0\",\"modType\":\"System.Single\",\"additionalRules\":\"NotSet\",\"targetCollection\":\"NotSet\",\"targetWeaponCategory\":\"NotSet\",\"targetWeaponType\":\"NotSet\",\"targetAmmoCategory\":\"NotSet\",\"targetWeaponSubType\":\"NotSet\"},\"tagData\":null,\"floatieData\":null,\"actorBurningData\":null,\"vfxData\":null,\"instantModData\":null,\"poorlyMaintainedEffectData\":null}]";
+      private static string PPC_STATUS = "{\"durationData\":{\"duration\":1,\"ticksOnActivations\":true,\"useActivationsOfTarget\":true,\"ticksOnEndOfRound\":false,\"ticksOnMovements\":false,\"stackLimit\":0,\"clearedWhenAttacked\":false},\"targetingData\":{\"effectTriggerType\":\"OnHit\",\"triggerLimit\":0,\"extendDurationOnTrigger\":0,\"specialRules\":\"NotSet\",\"effectTargetType\":\"NotSet\",\"range\":0,\"forcePathRebuild\":false,\"forceVisRebuild\":false,\"showInTargetPreview\":true,\"showInStatusPanel\":true},\"effectType\":\"StatisticEffect\",\"Description\":{\"Id\":\"AbilityDefPPC\",\"Name\":\"SENSORS IMPAIRED\",\"Details\":\"[AMT] Difficulty to all of this unit's attacks until its next activation.\",\"Icon\":\"uixSvgIcon_status_sensorsImpaired\"},\"nature\":\"Debuff\",\"statisticData\":{\"appliesEachTick\":false,\"effectsPersistAfterDestruction\":false,\"statName\":\"AccuracyModifier\",\"operation\":\"Float_Add\",\"modValue\":\"1.0\",\"modType\":\"System.Single\",\"additionalRules\":\"NotSet\",\"targetCollection\":\"NotSet\",\"targetWeaponCategory\":\"NotSet\",\"targetWeaponType\":\"NotSet\",\"targetAmmoCategory\":\"NotSet\",\"targetWeaponSubType\":\"NotSet\"},\"tagData\":null,\"floatieData\":null,\"actorBurningData\":null,\"vfxData\":null,\"instantModData\":null,\"poorlyMaintainedEffectData\":null}";
    }
 }
