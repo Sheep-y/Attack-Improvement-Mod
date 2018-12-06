7447d75c79976d5791a7468dae6da97ce19f21cb
Sheep-y <your.sheepy@gmail.com>
2018-12-07 02:01:51 +0800

    More laser stats fixes and holly srm correction.

--- AttackImprovementMod/src/modules/GameData.cs
+++ AttackImprovementMod/src/modules/GameData.cs
@@ -14,7 +14,9 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
          weaponsToFix = new HashSet<string>( new string[]{ 
             "Weapon_LRM_LRM5_2-Delta", "Weapon_LRM_LRM10_2-Delta", "Weapon_LRM_LRM15_2-Delta", // Since Launch
             "Weapon_SRM_SRM6_1-Holly", "Weapon_SRM_SRM6_2-Holly", // 1.0 to 1.2.1, fixed in 1.3
-            "Weapon_Laser_LargeLaserER_2-BlazeFire", "Weapon_PPC_PPCER_0-STOCK", "Weapon_PPC_PPCER_1-MagnaFirestar", "Weapon_PPC_PPCER_2-TiegartMagnum" } ); // Patch 1.3
+            "Weapon_Laser_MediumLaserER_2-BrightBloom", "Weapon_Laser_SmallLaserER_2-BlazeFire", "Weapon_Laser_LargeLaserER_2-BlazeFire", // Patch 1.3
+            "Weapon_Laser_LargeLaserPulse_2-Exostar", "Weapon_Laser_MediumLaserPulse_2-Magna400P", // Patch 1.3
+            "Weapon_PPC_PPCER_0-STOCK", "Weapon_PPC_PPCER_1-MagnaFirestar", "Weapon_PPC_PPCER_2-TiegartMagnum" } ); // Patch 1.3
          Patch( typeof( WeaponDef ), "FromJSON", null, "FixWeaponStats" );
       }
 
@@ -45,12 +47,20 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
             case "Weapon_SRM_SRM6_1-Holly":
             case "Weapon_SRM_SRM6_2-Holly":
                if ( CanFixWeapon( id ) )
-                  TryFixProp( id, __instance, "AccuracyModifier", e => e.AccuracyModifier > 0 && e.BonusValueB == "", 0 );
+                  TryFixProp( id, __instance, "AccuracyModifier", e => e.AccuracyModifier < 0 && e.BonusValueB == "", 0 );
                break;
 
+            case "Weapon_Laser_SmallLaserER_2-BlazeFire":
+            case "Weapon_Laser_MediumLaserER_2-BrightBloom":
             case "Weapon_Laser_LargeLaserER_2-BlazeFire":
                if ( CanFixWeapon( id ) )
-                  TryFixProp( id, __instance, "BonusValueA", e => e.BonusValueA == "" && e.BonusValueB == "", "+ 5% Crit" );
+                  TryFixProp( id, __instance, "BonusValueA", e => e.CriticalChanceMultiplier == 1.3f && e.BonusValueA == "" && e.BonusValueB == "", "+ 5% Crit." );
+               break;
+
+            case "Weapon_Laser_LargeLaserPulse_2-Exostar":
+            case "Weapon_Laser_MediumLaserPulse_2-Magna400P":
+               if ( CanFixWeapon( id ) )
+                  TryFixProp( id, __instance, "BonusValueA", e => e.AccuracyModifier == -3 && e.BonusValueA == "" && e.BonusValueB == "", "+ 1 Acc." );
                break;
 
             case "Weapon_PPC_PPCER_0-STOCK":
@@ -82,7 +92,7 @@ namespace Sheepy.BattleTechMod.AttackImprovementMod {
             prop.SetValue( def, value, null );
             Info( id + " " + propName + " corrected." );
          } else
-            Warn( id + " " + propName + " not corrected due to stats or property mismatch." );
+            Info( id + " " + propName + " seems to be already fixed." );
       }
 
       private static string PPC_STATUS = "{\"durationData\":{\"duration\":1,\"ticksOnActivations\":true,\"useActivationsOfTarget\":true,\"ticksOnEndOfRound\":false,\"ticksOnMovements\":false,\"stackLimit\":0,\"clearedWhenAttacked\":false},\"targetingData\":{\"effectTriggerType\":\"OnHit\",\"triggerLimit\":0,\"extendDurationOnTrigger\":0,\"specialRules\":\"NotSet\",\"effectTargetType\":\"NotSet\",\"range\":0,\"forcePathRebuild\":false,\"forceVisRebuild\":false,\"showInTargetPreview\":true,\"showInStatusPanel\":true},\"effectType\":\"StatisticEffect\",\"Description\":{\"Id\":\"AbilityDefPPC\",\"Name\":\"SENSORS IMPAIRED\",\"Details\":\"[AMT] Difficulty to all of this unit's attacks until its next activation.\",\"Icon\":\"uixSvgIcon_status_sensorsImpaired\"},\"nature\":\"Debuff\",\"statisticData\":{\"appliesEachTick\":false,\"effectsPersistAfterDestruction\":false,\"statName\":\"AccuracyModifier\",\"operation\":\"Float_Add\",\"modValue\":\"1.0\",\"modType\":\"System.Single\",\"additionalRules\":\"NotSet\",\"targetCollection\":\"NotSet\",\"targetWeaponCategory\":\"NotSet\",\"targetWeaponType\":\"NotSet\",\"targetAmmoCategory\":\"NotSet\",\"targetWeaponSubType\":\"NotSet\"},\"tagData\":null,\"floatieData\":null,\"actorBurningData\":null,\"vfxData\":null,\"instantModData\":null,\"poorlyMaintainedEffectData\":null}";
